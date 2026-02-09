25300768a058d8d2aa9f6b2e97f4b59af03a62d3
T8et <devt8et2022@gmail.com>
2026-02-09 22:59:26 +0630

    ADO.Net Start

--- DotNetTrainingBatch5.ConsoleApp/Program.cs
+++ DotNetTrainingBatch5.ConsoleApp/Program.cs
@@ -1,6 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("");
 Console.ReadKey();
 
 // md = markdown
+
+// C# database to connet
+
+// ADO.ent
+// Dapper (ORM - Object Relational Mapping)
+// EF Core/ Entity Framework - ORM
+
+// C# -> query
+
+// nuget 
+
+// Ctrl + .
+// only ADO.net need to close connection
+string connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=p@ssw0rd";
+Console.WriteLine(value: "Connection String:"+connectionString);
+SqlConnection connection = new SqlConnection(connectionString);
+
+Console.WriteLine("Connection Opening...");
+connection.Open();
+Console.WriteLine("Connection Opened");
+Console.WriteLine("Connection Closing");
+connection.Close();
+Console.WriteLine("Connection Closed");
