bce5396bc5c613ad74c141cdad522ceee7176b6c
T8et <devt8et2022@gmail.com>
2026-02-10 23:17:22 +0630

    DataAdapter, DataReader

--- DotNetTrainingBatch5.ConsoleApp/Program.cs
+++ DotNetTrainingBatch5.ConsoleApp/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("");
+//Console.WriteLine("");
 Console.ReadKey();
 
 // md = markdown
@@ -26,6 +27,43 @@ SqlConnection connection = new SqlConnection(connectionString);
 Console.WriteLine("Connection Opening...");
 connection.Open();
 Console.WriteLine("Connection Opened");
+
+//Command = Query
+string query = @"SELECT * FROM TBL_BLOG";
+SqlCommand cmd = new SqlCommand(query,connection);
+#region "DataAdapter"
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//adapter.Fill(dt);
+#endregion
+
+#region "DataReader"
+SqlDataReader reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+    Console.WriteLine(reader["BlogId"] + "|" + reader["BlogTitle"] + "|" + reader["BlogAuthor"] + "|" + reader["BlogContent"]);
+}
+#endregion
+
+//dt = adapter.execute();
+//for vs foreach (foreach better)
+
+//DataSet
+//DataTable
+//DataRow
+//DataColumn
+
 Console.WriteLine("Connection Closing");
 connection.Close();
 Console.WriteLine("Connection Closed");
+
+//C# mhr data table htae pp so yin con close htr lae use loh ya tl
+#region "DataAdapter"
+//foreach (DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine(dr["BlogId"] + "|" + dr["BlogTitle"] + "|" + dr["BlogAuthor"] + "|" + dr["BlogContent"]);
+//}
+#endregion
+
+//SqlDataAdapter >> for group dataset (not many amount number should use)
+//SqlDataReader  >> for performance, can use line by line read (not for some group of dataset usage)
